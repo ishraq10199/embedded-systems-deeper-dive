d30213695a0e09f178ae4fdc0aca3da313ae9ce4
Ishraq Hasan <ishraq10199@gmail.com>
2026-04-30 11:45:36 +0600

    #30 Apr 2026: Bootloader - Added UART welcome message to bootloader

--- STM32/projects/tooling/1_custom_bootloader/boot/src/bootloader.c
+++ STM32/projects/tooling/1_custom_bootloader/boot/src/bootloader.c
@@ -1,5 +1,117 @@
 #include "memory_map.h"
+#include "stm32f411xe.h"
 #include <inttypes.h>
+
+/**
+ ** ***********************
+ ** ** UART CODE - START **
+ ** ***********************
+ */
+
+#define SYS_FREQ 16000000
+#define UART_BAUDRATE 115200
+
+#define APB1CLK SYS_FREQ
+
+#define GPIOAEN (1U << 0)
+#define USART2EN (1U << 17)
+
+#define CR1_TE (1U << 3)
+#define CR1_RE (1U << 2)
+#define CR1_UE (1U << 13)
+
+#define SR_RXNE (1U << 5)
+#define SR_TC (1U << 6)
+#define SR_TXE (1U << 7)
+
+static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate);
+static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
+
+void uart2_write(int ch) {
+    while (!(USART2->SR & SR_TXE)) {
+    }
+    USART2->DR = (ch & 0xFF);
+}
+
+// int __io_putchar(int ch) {
+//     uart2_write(ch);
+//     return ch;
+// }
+
+static void uart2_puts(const char *s) {
+    while (*s)
+        uart2_write(*s++);
+}
+
+void uart2_tx_init(void) {
+    /*************** Configure USART GPIO Pin ***************/
+    /* Enable clock access to GPIOA (for PA2) */
+    RCC->AHB1ENR |= GPIOAEN;
+
+    /* Set PA2 mode to alternate function mode */
+    GPIOA->MODER &= ~(1U << 4);
+    GPIOA->MODER |= (1U << 5);
+
+    /* Set PA2 alternate function type to USART_TX */
+    GPIOA->AFR[0] |= (1U << 8);
+    GPIOA->AFR[0] |= (1U << 9);
+    GPIOA->AFR[0] |= (1U << 10);
+    GPIOA->AFR[0] &= ~(1U << 11);
+
+    /*************** Configure USART Module ***************/
+    /* Enable clock access to USART2 */
+    RCC->APB1ENR |= USART2EN;
+
+    /* Configure baudrate */
+    uart_set_baudrate(USART2, APB1CLK, UART_BAUDRATE);
+
+    /* Configure transfer direction */
+    USART2->CR1 = CR1_TE;
+
+    /* Enable UART module */
+    USART2->CR1 |= CR1_UE;
+}
+
+char uart2_read(void) {
+    /* Ensure Data Register is NOT empty (check if SR_RXNE is set) */
+    while (!(USART2->SR & SR_RXNE)) {
+    }
+
+    /* Once it is, return the value in the data register*/
+    return USART2->DR;
+}
+
+static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate) {
+    USARTx->BRR = compute_uart_bd(PeriphClk, BaudRate);
+}
+
+static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate) {
+    return ((PeriphClk + (BaudRate / 2U)) / BaudRate);
+}
+
+static void uart2_tx_deinit() {
+    /* Wait for the shift register to finish clocking out the last byte */
+    while (!(USART2->SR & SR_TC)) {
+    }
+
+    /* De-initialize the UART module, i.e. do the reverse of init */
+    USART2->CR1 = 0;
+    USART2->BRR = 0;
+    GPIOA->AFR[0] &= ~(1U << 8);
+    GPIOA->AFR[0] &= ~(1U << 9);
+    GPIOA->AFR[0] &= ~(1U << 10);
+    GPIOA->AFR[0] &= ~(1U << 11);
+    GPIOA->MODER &= ~(1U << 4);
+    GPIOA->MODER &= ~(1U << 5);
+    RCC->APB1ENR &= ~(USART2EN);
+    RCC->AHB1ENR &= ~GPIOAEN;
+}
+
+/**
+ ** ***********************
+ ** *** UART CODE - END ***
+ ** ***********************
+ */
 
 /**
  ** Load R1 into MSP (msr => Move immediate value to System Register)
@@ -12,10 +124,19 @@ __attribute__((naked)) static void start_app(uint32_t pc, uint32_t sp) {
     ");
 }
 
-/* This would go into .data's initializer varaibles, but if we exclude this from the binary, how will it work? */
+/* Force this variable in our build, to inspect if the data stays */
 volatile uint32_t myvar = 0x12345678;
 
 int main(void) {
+
+    /* UART message out */
+    uart2_tx_init();
+    uart2_puts("********************************\r\n");
+    uart2_puts("**** BOOTLOADER v0 SAYS HI! ****\r\n");
+    uart2_puts("********************************\r\n");
+    uart2_puts("\r\n");
+    uart2_tx_deinit();
+
     /* Get the pointer to where the app is in ROM */
     uint32_t *app_code = (uint32_t *)&__approm_start__;
 
